eb21e59f85266cdfae878fed624f7e84bb8e36b5
Amaury Diaz <a.caballero@lab.matcom.uh.cu>
2016-12-27 17:57:15 -0500

    Trie tested

--- src/test.c
+++ src/test.c
@@ -7,11 +7,13 @@ int main(int argc, char const *argv[]) {
   int a = trie_add(trie, "warehouse", 18293);
   int b = trie_contains(trie, "warehouse");
   void *v = trie_get(trie, "warehouse");
-  printf("%d %d %d\n", a, b, v);
 
-  for(char i = 0; i < 123; i++){
-    if(trie_pos(i) != i)
-      printf("%c\n", i);
-  }
+  int a2 = trie_add(trie, "warehous", "subterfuge");
+  int b3 = trie_contains(trie, "warehous");
+  void *v4 = trie_get(trie, "warehous");
+  
+  printf("%d %d %d\n", a, b, v);
+  printf("%d %d %s\n", a2, b3, v4);
+
   return 0;
 }
