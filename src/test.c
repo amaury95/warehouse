9a85fc955809db1ff3c053496a122481346b6b0c
Amaury Diaz <amauryuh@gmail.com>
2016-12-27 14:29:54 -0800

    Writted some test

---
+++ src/test.c
@@ -0,0 +1,11 @@
+#include "include/cJSON.h"
+#include "include/structs.h"
+
+int main(int argc, char const *argv[]) {
+  TRIE *trie = trie_new();
+  int a = trie_add(trie, "warehouse", 18293);
+  int b = trie_contains(trie, "warehouse");
+  void *v = trie_get(trie, "warehouse");
+  printf("%d %d %d\n", a, b, v);
+  return 0;
+}
